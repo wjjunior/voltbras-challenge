6bb285c5fd89d9ecc766d6f4f1e3f3f8e7a00ba8
Wagner <wagner.junior30@gmail.com>
2020-09-10 00:00:16 -0300

    feat: ensure LoadPlanetsController returns 200 on success

--- src/presentation/controllers/planet/load-planets/load-planets-controller.ts
+++ src/presentation/controllers/planet/load-planets/load-planets-controller.ts
@@ -1,9 +1,10 @@
 import { Controller, HttpRequest, HttpResponse, LoadPlanets } from './load-planets-controller-protocols'
+import { ok } from '../../../helpers/http/http-helper'
 
 export class LoadPlanetsController implements Controller {
   constructor (private readonly loadPlanets: LoadPlanets) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadPlanets.load()
-    return null
+    const planets = await this.loadPlanets.load()
+    return ok(planets)
   }
 }
