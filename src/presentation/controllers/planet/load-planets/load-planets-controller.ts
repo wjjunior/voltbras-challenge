bf25049c59959b5de1afa269d8dba5b9537cc3d3
Wagner <wagner.junior30@gmail.com>
2020-09-10 00:06:30 -0300

    feat: ensure LoadPlanetsController returns 500 if LoadPlanetsUseCase throws

--- src/presentation/controllers/planet/load-planets/load-planets-controller.ts
+++ src/presentation/controllers/planet/load-planets/load-planets-controller.ts
@@ -1,10 +1,15 @@
 import { Controller, HttpRequest, HttpResponse, LoadPlanets } from './load-planets-controller-protocols'
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 
 export class LoadPlanetsController implements Controller {
   constructor (private readonly loadPlanets: LoadPlanets) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const planets = await this.loadPlanets.load()
-    return ok(planets)
+    try {
+      const planets = await this.loadPlanets.load()
+      return ok(planets)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
