0271d04da04af90a1f63580ebfa0842d59020954
Wagner <wagner.junior30@gmail.com>
2020-09-10 18:15:39 -0300

    feat: ensure PlanetRepository load all planets

---
+++ src/infra/db/arcsecond/helpers/arcsecond-helper.ts
@@ -0,0 +1,13 @@
+import { RESTDataSource } from 'apollo-datasource-rest'
+
+export default class ArcsecondHelper extends RESTDataSource {
+  constructor () {
+    super()
+    this.baseURL = 'https://api.arcsecond.io/'
+  }
+
+  async getRequest (uri: string): Promise<any> {
+    const response = await this.get(`${this.baseURL}${uri}`)
+    return JSON.parse(response)
+  }
+}
