db29a8be70eb280becb510e27c7e7e1e004aa62d
Wagner <wagner.junior30@gmail.com>
2020-09-12 14:17:38 -0300

    refactor: improve PlanetRepository tests

--- src/infra/db/arcsecond/planet/planet-repository.spec.ts
+++ src/infra/db/arcsecond/planet/planet-repository.spec.ts
@@ -1,39 +1,41 @@
 import { PlanetRepository } from './planet-repository'
 import ArcsecondApi from '../helpers/arcsecond-api'
 
-beforeAll(() => {
-  jest.spyOn(ArcsecondApi.prototype, 'getRequest').mockImplementation(
-    async () =>
-      new Promise((resolve) =>
-        resolve({
-          results: [
-            {
-              name: 'any_name',
-              mass: 123
-            },
-            {
-              name: 'other_name',
-              mass: 321
-            }
-          ]
-        })
-      )
-  )
-})
-
-afterAll(() => {
-  jest.restoreAllMocks()
-})
-
-const makeSut = (): PlanetRepository => {
-  const arcsecondApi = new ArcsecondApi()
-  return new PlanetRepository(arcsecondApi)
+interface SutTypes {
+  sut: PlanetRepository
+  arcsecondApiStub: ArcsecondApi
+}
+const makeSut = (): SutTypes => {
+  const arcsecondApiStub = new ArcsecondApi()
+  const sut = new PlanetRepository(arcsecondApiStub)
+  return {
+    sut,
+    arcsecondApiStub
+  }
 }
 
 describe('Planet Arcsecond Repository', () => {
   describe('loadAll()', () => {
     test('Should load all planets on success', async () => {
-      const sut = makeSut()
+      const { sut, arcsecondApiStub } = makeSut()
+      jest.spyOn(arcsecondApiStub, 'getRequest').mockReturnValueOnce(new Promise((resolve) => resolve({
+        results: [
+          {
+            name: 'any_name',
+            mass: {
+              unit: 'any_unit',
+              value: 123
+            }
+          },
+          {
+            name: 'other_name',
+            mass: {
+              unit: 'any_unit',
+              value: 321
+            }
+          }
+        ]
+      })))
       const planets = await sut.loadAll()
       expect(planets.length).toBe(2)
     })
