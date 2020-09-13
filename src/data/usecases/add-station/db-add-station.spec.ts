346209d278234561e704bb382b143cd38a94bf5b
Wagner <wagner.junior30@gmail.com>
2020-09-13 15:59:30 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/add-station/db-add-station.spec.ts
+++ src/data/usecases/add-station/db-add-station.spec.ts
@@ -1,24 +1,48 @@
 import { DbAddStation } from './db-add-station'
-import { AddStationModel, AddStationRepository, StationModel } from './db-add-station-protocols'
+import {
+  AddStationModel,
+  AddStationRepository,
+  StationModel
+} from './db-add-station-protocols'
 
 const makeFakeStationData = (): AddStationModel => ({
   name: 'any_name',
   planet: 'any_planet'
 })
-describe('DbAddPlanet', () => {
-  test('Should call AddStationRepository with correct values', async () => {
-    class AddStationRepositoryStub implements AddStationRepository {
-      async add (data: AddStationModel): Promise<StationModel> {
-        return new Promise(resolve => resolve({
+
+const makeAddStationRepository = (): AddStationRepository => {
+  class AddStationRepositoryStub implements AddStationRepository {
+    async add (data: AddStationModel): Promise<StationModel> {
+      return new Promise((resolve) =>
+        resolve({
           id: 1,
           name: 'any_name',
           planet: 'any_planet'
-        }))
-      }
+        })
+      )
     }
-    const addStationRepositoryStub = new AddStationRepositoryStub()
+  }
+  return new AddStationRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAddStation
+  addStationRepositoryStub: AddStationRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addStationRepositoryStub = makeAddStationRepository()
+  const sut = new DbAddStation(addStationRepositoryStub)
+  return {
+    sut,
+    addStationRepositoryStub
+  }
+}
+
+describe('DbAddPlanet', () => {
+  test('Should call AddStationRepository with correct values', async () => {
+    const { sut, addStationRepositoryStub } = makeSut()
     const addSpy = jest.spyOn(addStationRepositoryStub, 'add')
-    const sut = new DbAddStation(addStationRepositoryStub)
     const stationData = makeFakeStationData()
     await sut.add(stationData)
     expect(addSpy).toHaveBeenCalledWith(stationData)
