058bf30eba1c42f7a9788d582105467858328703
Wagner <wagner.junior30@gmail.com>
2020-09-13 18:08:57 -0300

    chore: configure prisma for tests with jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
+
+const path = require('path')
+
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts'
   ],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
+  testEnvironment: path.join(__dirname, './prisma/prisma-test-environment.js'),
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
