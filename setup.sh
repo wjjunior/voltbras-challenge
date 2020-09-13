aa77487293b72dcd056035e04b0c40c4c29ccfda
Wagner <wagner.junior30@gmail.com>
2020-09-13 09:50:47 -0300

    chore: configure setup

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+echo "Options: ["
+echo "    1 - build"
+echo "    2 - start containers"
+echo "    3 - stop containers"
+echo "    4 - restart containers"
+echo "    5 - rebuild"
+echo "    6 - destroy"
+echo "]"
+read -p "Select an option: " option
+
+configure_app() {
+    npx prisma migrate save --experimental --name "init"
+}
+
+if [[ option -eq 1 ]]; then
+    docker-compose up --detach --build
+
+    configure_app
+fi
+
+if [[ option -eq 2 ]]; then
+    docker-compose start
+fi
+
+if [[ option -eq 3 ]]; then
+    docker-compose stop
+fi
+
+if [[ option -eq 4 ]]; then
+    docker-compose restart
+fi
+
+if [[ option -eq 5 ]]; then
+    docker-compose down
+    docker-compose up --detach --build
+
+    configure_app
+fi
+
+if [[ option -eq 6 ]]; then
+    docker-compose down
+fi
