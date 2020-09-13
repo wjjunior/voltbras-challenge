8f0e8a1db9107ee7d478362a20f3d2332d7b68e4
Wagner <wagner.junior30@gmail.com>
2020-09-13 12:17:42 -0300

    feat: ensure AddStationController returns a badRequest error if validation fails

--- src/presentation/controllers/station/add-station/add-station-controller.ts
+++ src/presentation/controllers/station/add-station/add-station-controller.ts
@@ -1,4 +1,5 @@
 import { Controller, HttpRequest, HttpResponse, Validation } from './add-station-controller-protocols'
+import { badRequest } from '../../../../presentation/helpers/http/http-helper'
 
 export class AddStationController implements Controller {
   constructor (
@@ -6,7 +7,10 @@ export class AddStationController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
     return new Promise(resolve => resolve(null))
   }
 }
